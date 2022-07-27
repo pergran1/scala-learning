ee9f137535a95bd33ca1dbbda0bfc668cea30d7c
pergran1 <pergranberg@hotmail.com>
2022-07-27 16:27:11 +0200

    added line

--- src/main/scala/playground/Playground.scala
+++ src/main/scala/playground/Playground.scala
@@ -4,6 +4,7 @@ object Playground {
 
   def main(args: Array[String]): Unit = {
     println("hejsan, jag är redo att lära mig scala!!!")
+    println("Is this really scala and can I add this to github?")
   }
 
 }
