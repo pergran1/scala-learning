9395886b1c75422af341141731e928f3d42739b8
pergran1 <pergranberg@hotmail.com>
2022-07-28 20:09:07 +0200

    added tail rec

---
+++ src/main/scala/lectures/call_by_value.scala
@@ -0,0 +1,22 @@
+package lectures
+// https://www.udemy.com/course/rock-the-jvm-scala-for-beginners/learn/lecture/7660632#overview
+object call_by_value extends App{
+    def called_by_value(x: Long): Unit = {
+      println(f"by value: $x")
+      println(f"by value: $x")
+    }
+
+  def called_by_name(x: => Long): Unit = {
+    println(f"by name: $x")
+    println(f"by name: $x")
+  }
+
+  called_by_value(System.nanoTime())  //this returns on the same time, is evaluated before and same time is used
+  called_by_name(System.nanoTime()) // this does not, evaluated every time so the time is changed
+  //called by name is good in lazy streams
+
+  def infinite(): Int = 1 + infinite()
+  def print_first(x: Int, y: => Int) = println(x)
+
+  print_first( 34, infinite())  //does not crash because infinite is never used, it never evaluates
+}
