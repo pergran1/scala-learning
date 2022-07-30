baa3c9706a660b7c68a42708ca00c429101f5426
pergran1 <pergranberg@hotmail.com>
2022-07-30 17:56:37 +0200

    Added package

--- src/main/scala/lectures/part2OOP/Exceptions.scala
+++ src/main/scala/lectures/part2OOP/Exceptions.scala
@@ -17,15 +17,26 @@ object Exceptions  extends App{
     if (withExceptions) throw new RuntimeException("No in for you")
     else 42
 
-  try {
+  // dont need to create a val
+  val potential_fail = try {
     //code that might throw
     get_int(true)
   } catch {
+    // select what should happen
     case e: RuntimeException => println("Caught a Runtime exception")
   } finally {
     // code that will always run
+    // this finally block is optional
+    // does not influence the return type of this exception
+    // use finally only for side effects, example logging
     println("Finally!!")
   }
+
+  // 3. How to define your own exceptions
+  class my_exception extends Exception
+  val exception = new my_exception
+  
+  throw exception
 
 
 
---
+++ src/main/scala/lectures/part2OOP/PackageAndImports.scala
@@ -0,0 +1,11 @@
+package lectures.part2OOP
+
+object PackageAndImports extends App{
+
+  // this uses a class from the file "OOPbasics", is in the same package
+  val writer = new Writer("Pelle", "RockTheJvM", 2018)
+
+
+  // packages are in hierarchy
+  // matching folder structure
+}
